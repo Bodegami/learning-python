c796a6e9e72192dcbb28fc655553b030c48650f7
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-05-06 16:38:16 -0300

    sobre o elif & melhorando a legibilidade do codigo

--- formacao-python-alura/jogos/adivinhacao.py
+++ formacao-python-alura/jogos/adivinhacao.py
@@ -2,25 +2,25 @@ print("****************************************")
 print("  Bem vindo ao jogo de Adivinhação!!")
 print("****************************************")
 
-numero_secreto = 42 ## Variaveis tem o tipo dinamico (ele atribiu através da inferência)
-chute_str = input("Digite o seu numero: ") ## A variavel do input recebe o valor digitado
+numero_secreto = 42
 
-tipo = type(chute_str) ## No python 3 o input devolve um tipo string
-print("o tipo do input é:", tipo)
-
-chute_int = int(chute_str)  ## Podemos fazer o parse através do metodo int()
-
-# Poderiamos fazer assim também: chute = int(input("Digite o seu numero: "))
+chute_str = input("Digite o seu numero: ")
 
 print("Você digitou: ", chute_str)
 
-## Na linguagem python, para indicar que é um bloco de execucao usamos os :
-## Além disso, uma caracteristica importante da linguagem é a endentação, que espera 4 espaços
-if (numero_secreto == chute_int):
+chute_int = int(chute_str)
+
+acertou = chute_int == numero_secreto
+maior   = chute_int > numero_secreto
+menor   = chute_int < numero_secreto
+
+if (acertou):
     print("Você acertou!!")
 else:
-    print("Você errou!")
-
+    if (maior):
+        print("Você errou! O seu chute foi maior do que o número secreto.")
+    elif (menor):
+        print("Você errou! O seu chute foi menor do que o número secreto.")
 
 
 
