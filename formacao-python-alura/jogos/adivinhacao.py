d66a0b92ecff133f434afc90b464a08fa0b3b59b
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-05-07 22:45:51 -0300

    sobre o break, continue & operadores lógicos

--- formacao-python-alura/jogos/adivinhacao.py
+++ formacao-python-alura/jogos/adivinhacao.py
@@ -8,9 +8,13 @@ rodada = 1
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu numero: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou: ", chute_str)
     chute_int = int(chute_str)
+
+    if chute_int < 1 or chute_int > 100:
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute_int == numero_secreto
     maior   = chute_int > numero_secreto
@@ -18,12 +22,13 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if (acertou):
         print("Você acertou!!")
+        break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif (menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
-
+print("Fim do jogo!")
 
 
 
