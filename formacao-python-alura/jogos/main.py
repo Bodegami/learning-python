c18c856325b628ba474bae5285829f7df6f8fd27
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-05-07 22:58:49 -0300

    mais sobre interpolação de strings

--- formacao-python-alura/jogos/main.py
+++ formacao-python-alura/jogos/main.py
@@ -22,3 +22,14 @@ jogo = "Adivinhação"
 
 print("Do que é esse jogo?", jogo)
 print("Bem", "vindo", "no", "jogo", "de", jogo, sep="-")
+
+print("Tentativa {1} de {1}".format(3,10))
+print("R$ {:f}".format(1.59))
+print("R$ {:.2f}".format(1.59))
+print("R$ {:.2f}".format(1234.5))
+print("R$ {:7.2f}".format(1234.5))
+print("R$ {:7.2f}".format(4.5))
+print("R$ {:07.2f}".format(4.5))
+print("R$ {:07d}".format(46))
+print("Data {:02d}/{:02d}".format(9,11))
+
