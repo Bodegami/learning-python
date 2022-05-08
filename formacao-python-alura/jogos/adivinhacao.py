2853510c3abc3df8c452f443ecab5cc64d336391
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-05-07 23:42:47 -0300

    adicionando pontuação a partida & sobre numeros absolutos abs()

--- formacao-python-alura/jogos/adivinhacao.py
+++ formacao-python-alura/jogos/adivinhacao.py
@@ -6,12 +6,12 @@ print("****************************************")
 
 numero_secreto = random.randrange(1,101)
 total_de_tentativas = 0
+pontos = 1000
 
 print("Qual o nível de dificuldade?")
 print("(1) Fácil (2) médio (3) difícil")
 
 nivel = int(input("Define um nível: "))
-
 if nivel == 1:
     total_de_tentativas = 20
 elif nivel == 2:
@@ -21,6 +21,7 @@ else:
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+
     chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou: ", chute_str)
     chute_int = int(chute_str)
@@ -34,13 +35,16 @@ for rodada in range(1, total_de_tentativas + 1):
     menor   = chute_int < numero_secreto
 
     if (acertou):
-        print("Você acertou!!")
+        print("Você acertou e fez {} pontos!!!".format(pontos))
         break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif (menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute_int)
+        pontos = pontos - pontos_perdidos
+
 print("Fim do jogo!")
 print("O número secreto é", numero_secreto)
 
