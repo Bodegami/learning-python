b2b9ca5c44c59950a1da4113be0220dd40354aed
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-05-08 23:40:27 -0300

    sobre funções

--- formacao-python-alura/jogos/jogos.py
+++ formacao-python-alura/jogos/jogos.py
@@ -1,17 +1,21 @@
 import forca
 import adivinhacao
 
-print("****************************************")
-print("*********Escolha o seu jogo!************")
-print("****************************************")
+def escolhe_jogo():
+    print("****************************************")
+    print("*********Escolha o seu jogo!************")
+    print("****************************************")
 
-print("(1) Forca | (2) adivinhação")
+    print("(1) Forca | (2) adivinhação")
 
-jogo = int(input("Qual o jogo? "))
+    jogo = int(input("Qual o jogo? "))
 
-if jogo == 1:
-    print("Jogando Forca")
-    forca.jogar()
-elif jogo == 2:
-    print("Jogando Adivinhação")
-    adivinhacao.jogar()
+    if jogo == 1:
+        print("Jogando Forca")
+        forca.jogar()
+    elif jogo == 2:
+        print("Jogando Adivinhação")
+        adivinhacao.jogar()
+
+if(__name__ == "__main__"):
+    escolhe_jogo()
