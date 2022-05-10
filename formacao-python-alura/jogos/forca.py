27da32be0d3cf185529629b12a94a080bcbdc7df
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-05-09 23:47:03 -0300

    python3 parte2 - avancando na linguagem

--- formacao-python-alura/jogos/forca.py
+++ formacao-python-alura/jogos/forca.py
@@ -1,11 +1,30 @@
 
 def jogar():
     print("****************************************")
-    print("  Bem vindo ao jogo de Forca!!")
+    print("******Bem vindo ao jogo de Forca!!******")
     print("****************************************")
+
+    palavra_secreto = "banana"
+
+    enforcou = False
+    acertou = False
+
+    
+    while(not enforcou and not acertou):
+        print("jogando....")
+
+
+
+
+
+
+
+
+
+
 
     print("Fim do jogo!")
 
-
 if (__name__ == "__main__"):
     jogar()
+
