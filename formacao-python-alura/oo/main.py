9f21668018a7b7f3f403d16ad5c93eac93e5ca75
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-05-15 22:28:31 -0300

    introducao a orientação a objetos com Python

---
+++ formacao-python-alura/oo/main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from teste import cria_conta
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+
+conta = cria_conta(123, "Nico", 55.0, 1000.0)
+print(conta)
