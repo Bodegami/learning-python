f7046e36a3ce5d672e39353c21cbe85c822b9252
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-05-19 23:30:46 -0300

    Revert "sobre modificares de acesso & getters and setters"
    This reverts commit a8fe857aa4c99503accde36df4fb6a076f4eb99a.

--- formacao-python-alura/oo2/modelo.py
+++ formacao-python-alura/oo2/modelo.py
@@ -1,47 +1,23 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.__likes = 0
+        self.likes = 0
 
     def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+        self.likes += 1
 
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.capitalize()
+        self.nome = nome.capitalize()
         self.ano = ano
         self.temporadas = temporadas
-        self.__likes = 0
+        self.likes = 0
 
     def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+        self.likes += 1
 
 
 
@@ -52,7 +28,6 @@ print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
       f'- Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
-atlanta.nome = 'atlanta - de grover'
 atlanta.dar_like()
 atlanta.dar_like()
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
