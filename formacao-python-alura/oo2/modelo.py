86b4d2b71110947e3ebac10a725ff6da6118ba06
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-05-24 22:22:29 -0300

    sobre coesao e print() por de baixo dos panos

--- formacao-python-alura/oo2/modelo.py
+++ formacao-python-alura/oo2/modelo.py
@@ -19,11 +19,17 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self._likes} Likes')
+
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self.duracao} min - {self._likes} Likes')
 
 
 class Serie(Programa):
@@ -31,24 +37,24 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes')
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-      f'- Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
-print(f'{vingadores.nome} - {vingadores.duracao} - {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.nome = 'atlanta - de groover'
 atlanta.dar_like()
 atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
-      f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
-print(f'{atlanta.nome} - {atlanta.temporadas} - {atlanta.likes}')
 
 
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
-    print(f'Nome: {programa.nome} - Detalhes: {detalhes} - Likes: {programa.likes}')
+    programa.imprime()
+
+
+print(123434) ## por de baixo dos panos o Python transforma o objeto numa representacao textual (string)
+print(str(123434)) ## é exatamente o que python faz no codigo acima
