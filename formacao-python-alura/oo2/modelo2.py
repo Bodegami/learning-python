050271584f162174845d3bfc5c3ed8c885484fb0
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-05-26 23:35:51 -0300

    sobre o conceito de classes MIXIN

--- formacao-python-alura/oo2/modelo2.py
+++ formacao-python-alura/oo2/modelo2.py
@@ -1,4 +1,7 @@
 class Funcionario:
+    def __init__(self, nome):
+        self.nome = nome
+
     def registra_horas(self, horas):
         print('Horas registradas...')
 
@@ -20,24 +23,37 @@ class Alura(Funcionario):
         print('Mostrando perguntas não respondidas do fórum')
 
 
+class Hipster:
+    def __str__(self):
+        return f'Hipster, {self.nome}'
+
 class Junior(Alura):
     pass
 
 class Pleno(Alura, Caelum):
     pass
 
-jose = Junior()
-jose.busca_perguntas_sem_resposta()
-jose.mostrar_tarefas()
+class Senior(Alura, Caelum, Hipster):
+    pass
 
-luan = Pleno()
-luan.busca_perguntas_sem_resposta()
-luan.busca_cursos_do_mes()
-
-luan.mostrar_tarefas()
+# jose = Junior()
+# jose.busca_perguntas_sem_resposta()
+# jose.mostrar_tarefas()
+#
+# luan = Pleno()
+# luan.busca_perguntas_sem_resposta()
+# luan.busca_cursos_do_mes()
+#
+# luan.mostrar_tarefas()
 
 ## MRO - Method Resolution Order
 ## algoritmo usado para tomada de decisao interna do Python 3
 
 
 # Pleno -> Alura -> Caelum -> Funcionario
+
+luan = Senior('Luan')
+print(luan)
+
+## MIXIN -> classe criada para compartilhar um comportamento e que será usada como herança
+## a ideia é não ter que instanciar uma classe mixin
