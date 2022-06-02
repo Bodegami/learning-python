eafa28fd951fb9a85a49025ff48d010f682a6693
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-06-01 23:43:56 -0300

    recuperando o valor de uma url com metodo find() & sobre len()

--- formacao-python-alura/extrator-url/main.py
+++ formacao-python-alura/extrator-url/main.py
@@ -1,4 +1,4 @@
-url = 'https://bytebank.com/cambio?moedaOrigem=real'
+url = 'https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real'
 print(url)
 
 ## O fatiamento tem uma propriedade interessante que é omitir o parametro
@@ -15,5 +15,23 @@ print(url_base)
 url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
 
-print(url)
+parametro_busca = 'moedaOrigem'
+indice_parametro = url.find(parametro_busca)
+tamanho_parametro = len(parametro_busca)
+indice_valor = indice_parametro + tamanho_parametro + 1
+valor = url[indice_valor:]
+print(valor)
+
+'''''
+ Buscando os valores no path
+ 
+    Primeiro eu crio uma variavel que recebe o nome do meu parametro como valor,
+    Depois eu busca a posicao no meu parametro na url
+    Depois eu pego o tamanho (size) do valor da minha variavel parametro_busca
+    Depois crio uma variavel indice_valor que vai somar a posicao do meu parametro, 
+    o tamanho da variavel busca e + 1 que representa o caractere '=' na minha url
+    Crio uma variavel valor que vai pega o restante da variavel url a partir da minha
+    variavel indice_valor 
+    
+'''''
 
---
+++ formacao-python-alura/extrator-url/playground.py
@@ -0,0 +1,27 @@
+from datetime import date, timedelta
+import random
+
+lista =[]
+data_hoje = date.today()
+
+count = random.randrange(1,31)
+
+data_mes_atras = (data_hoje - timedelta(31)).strftime('%Y-%m-%d')
+print(data_mes_atras)
+
+for data in range(1,31):
+    aleatorio = random.randrange(1, 31)
+    data_mes_atras = (data_hoje - timedelta(aleatorio)).strftime('%Y-%m-%d')
+    string_data = f"pedidos_de_{data_mes_atras}"
+    print(string_data)
+    lista.append(string_data)
+
+
+data_limite = '2022-05-22'
+
+for data in lista:
+    data_result = data.find('05-22')
+    if data == data_limite:
+        print("achou")
+
+
