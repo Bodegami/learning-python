79d086cf569d0d7445539a329129714b387ea6a7
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-06-01 21:00:51 -0300

    sobre o metodo find()

--- formacao-python-alura/extrator-url/main.py
+++ formacao-python-alura/extrator-url/main.py
@@ -1,15 +1,19 @@
-url = 'bytebank.com/cambio?moedaOrigem=real'
+url = 'https://bytebank.com/cambio?moedaOrigem=real'
 print(url)
 
-url_base = url[0:19]
+## O fatiamento tem uma propriedade interessante que é omitir o parametro
+## quando usado no inicio, ele pega o ponto inicial da string
+## quando usado no final, ele pega do ponto inicial até o final da variavel url
+
+# O metodo find busca um elemento dentro da string
+indice_interrogacao = url.find('?')
+
+
+url_base = url[0:indice_interrogacao]
 print(url_base)
 
-url_parametros = url[20:36]
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
 
 print(url)
 
-comida = 'fruta'
-comi = 'fruta'
-
-print(comida == comi)
