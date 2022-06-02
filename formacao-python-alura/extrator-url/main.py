8b833587ff7a9f325844da365315d53234c0b01c
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-06-02 00:05:39 -0300

    tornando mais flexivel a logica de busca no path

--- formacao-python-alura/extrator-url/main.py
+++ formacao-python-alura/extrator-url/main.py
@@ -1,19 +1,33 @@
-url = 'https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real'
+## url = 'https://bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real'
+url = 'https://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar'
 print(url)
 
 ## O fatiamento tem uma propriedade interessante que é omitir o parametro
 ## quando usado no inicio, ele pega o ponto inicial da string
 ## quando usado no final, ele pega do ponto inicial até o final da variavel url
 
-# O metodo find busca um elemento dentro da string
+# Separa a base e os parâmetros
+# Primeira solução estática
 indice_interrogacao = url.find('?')
-
-
 url_base = url[0:indice_interrogacao]
 print(url_base)
-
-url_parametros = url[indice_interrogacao+1:]
+url_parametros = url[indice_interrogacao + 1:]
 print(url_parametros)
+
+print("====================||Separa a base e os parâmetros||======================")
+
+'''''
+ Buscando os valores de moedaOrigem no path
+
+    Primeiro eu crio uma variavel que recebe o nome do meu parametro como valor,
+    Depois eu busca a posicao no meu parametro na url
+    Depois eu pego o tamanho (size) do valor da minha variavel parametro_busca
+    Depois crio uma variavel indice_valor que vai somar a posicao do meu parametro, 
+    o tamanho da variavel busca e + 1 que representa o caractere '=' na minha url
+    Crio uma variavel valor que vai pega o restante da variavel url a partir da minha
+    variavel indice_valor 
+
+'''''
 
 parametro_busca = 'moedaOrigem'
 indice_parametro = url.find(parametro_busca)
@@ -22,16 +36,17 @@ indice_valor = indice_parametro + tamanho_parametro + 1
 valor = url[indice_valor:]
 print(valor)
 
-'''''
- Buscando os valores no path
- 
-    Primeiro eu crio uma variavel que recebe o nome do meu parametro como valor,
-    Depois eu busca a posicao no meu parametro na url
-    Depois eu pego o tamanho (size) do valor da minha variavel parametro_busca
-    Depois crio uma variavel indice_valor que vai somar a posicao do meu parametro, 
-    o tamanho da variavel busca e + 1 que representa o caractere '=' na minha url
-    Crio uma variavel valor que vai pega o restante da variavel url a partir da minha
-    variavel indice_valor 
-    
-'''''
+print("====================||Busca o valor de um parâmetro||======================")
+# Busca o valor de um parâmetro
 
+# Adaptando a lógica para pegar o valor de qualquer parametro
+
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
