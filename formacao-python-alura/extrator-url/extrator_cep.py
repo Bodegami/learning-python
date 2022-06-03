c42be573eb3885c2be428f2045691b78ec893369
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-06-03 15:41:22 -0300

    sobre o RegEx - sobre agrupadores, limitadores, quantificadores e elementos opcionais

--- formacao-python-alura/extrator-url/extrator_cep.py
+++ formacao-python-alura/extrator-url/extrator_cep.py
@@ -6,7 +6,18 @@ import re # Regular Expression -- RegEx
 
 # Padrao em regex para o cep
 # o ? indica que o parametro é opcional
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+# padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+
+# sobre quantificadores - utilizamos {} e passamos o valor de quantas vezes os elementos de [] podem se repetir
+# padrao = re.compile("[0123456789]{5}[-]?[0123456789]{3}")
+
+# podemos simplificar ainda mais indicando que os algarismos são de 0 a 9 usando um hifen
+# padrao = re.compile("[0-9]{5}[-]?[0-9]{3}")
+
+# no lugar do ? podemos abrir chaves e dizer o minimo e maximo de vezes que aquele elemento pode aparecer,
+# dessa forma definimos um limite
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
+
 busca = padrao.search(endereco) # Retorna Match caso encontrado ou None caso não encontrado
 if busca:
     cep = busca.group() # retorna a String encontrada no padrão informado
