ea387ee55cd7c91114bc1368732559113c793acb
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-06-04 10:14:54 -0300

    desafio conversor dolar p/ real

--- formacao-python-alura/extrator-url/extrator_url.py
+++ formacao-python-alura/extrator-url/extrator_url.py
@@ -52,7 +52,7 @@ class ExtratorURL:
         return self.url == other.url
 
 
-url = 'https://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar'
+url = 'https://bytebank.com/cambio?quantidade=100&moedaOrigem=dolar&moedaDestino=real'
 extrator_url = ExtratorURL(url)
 extrator_url2 = ExtratorURL(url)
 print("O tamanho da URL: ", len(extrator_url))
@@ -68,7 +68,7 @@ print(id(extrator_url))
 print(id(extrator_url2))
 print(extrator_url is extrator_url2)
 
-print("=====================||||=========================")
+print("=====================|| Igualdade e Identidade||=========================")
 # A metodo de comparacao utilizando o id() por de baixo dos panos, funciona muito bem p/ strings
 # mas para objetos já não funciona como esperado, pq dentro da linguagem Python, dois objetos
 # podem ser iguais, mas não idênticos
@@ -79,3 +79,20 @@ print(id(string_teste))
 print(id(string_teste2))
 
 print(string_teste is string_teste2)
+
+print("=====================||Desafio conversor dolar||=========================")
+
+VALOR_DOLAR = 5.50  # 1 dólar = 5.50 reais
+moeda_origem = extrator_url.get_valor_parametro("moedaOrigem")
+moeda_destino = extrator_url.get_valor_parametro("moedaDestino")
+quantidade = extrator_url.get_valor_parametro("quantidade")
+
+if moeda_origem == "real" and moeda_destino == "dolar":
+    valor_conversao = int(quantidade) / VALOR_DOLAR
+    print("O valor de R$" + quantidade + " reais é igual a $" + str(valor_conversao) + " dólares.")
+elif moeda_origem == "dolar" and moeda_destino == "real":
+    valor_conversao = int(quantidade) * VALOR_DOLAR
+    print("O valor de $" + quantidade + " dólares é igual a R$" + str(valor_conversao) + " reais.")
+else:
+    print(f"Câmbio de {moeda_origem} para {moeda_destino} não está disponível.")
+
