740e873f6a0ddf6b4500c8c26af5abfd6a04324a
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-06-08 22:42:53 -0300

    ordenacao natural em nossas classes utilizando o __lt__()

--- formacao-python-alura/collections-listas-e-tuplas/ordenando_nossas_classes.py
+++ formacao-python-alura/collections-listas-e-tuplas/ordenando_nossas_classes.py
@@ -70,3 +70,56 @@ def extrai_saldo(conta):
 # o atributo que será usado para comparação
 for conta in sorted(contas, key=attrgetter('_codigo')):
     print(conta)
+
+
+print("\n===============|| Implementando a ordenacao natural na classe ContaSalario ||=================\n")
+print("\nImplementado metodo __lt__() == lessThan ou menor que")
+
+class ContaSalario:
+
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    def __lt__(self, other):
+        return self._saldo < other._saldo
+
+    def __str__(self):
+        return f"[>>Codigo {self._codigo} - Saldo {self._saldo}<<]"
+
+    def __eq__(self, other):
+        if type(other) != ContaSalario:
+            return False
+
+        return self._codigo == other._codigo and self._saldo == other._saldo
+
+
+print("\n==============|| Comparando nossos objetos com a nossa implementacao de ordenacao natural ||===============\n")
+
+
+conta_do_guilherme = ContaSalario(17)
+conta_do_guilherme.deposita(500)
+
+conta_da_daniela = ContaSalario(3)
+conta_da_daniela.deposita(1000)
+
+conta_do_paulo = ContaSalario(133)
+conta_do_paulo.deposita(510)
+
+contas = [conta_do_guilherme, conta_da_daniela, conta_do_paulo]
+imprime_contas = [print(conta) for conta in contas]
+print("\nCriterio de comparação: Saldo")
+print("\nA conta do Guilherme é menor que a conta da Daniela?")
+print(conta_do_guilherme < conta_da_daniela)
+print("\nA conta do Guilherme é menor que a conta da Daniela?")
+print(conta_do_guilherme < conta_da_daniela)
+
+# Note que agora não precisamos passar uma chave com um funcao ou attrgetter e
+# nem estamos acessando o atributo privado da classe
+# Essa abordagem é menos funcional e mais orientada a objetos
+print("\n")
+for conta in sorted(contas):
+    print(conta)
