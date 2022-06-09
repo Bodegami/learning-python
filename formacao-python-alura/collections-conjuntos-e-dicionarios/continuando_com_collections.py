517d3a028488212bcb726eb8a6ccfd287afc0a3a
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-06-09 15:53:37 -0300

    trabalhando com conjuntos/ set()

---
+++ formacao-python-alura/collections-conjuntos-e-dicionarios/continuando_com_collections.py
@@ -0,0 +1,79 @@
+# Lista tradicional
+usuarios_data_science = [15, 23, 43, 56]
+usuarios_machine_learning = [13, 23, 56, 42]
+
+print("\n=======================|| Metodo built-in copy() do list() ||=======================\n")
+
+
+# o metodo built-in copy() faz uma copia raza, ou seja, se a nossa lista fosse composta por objetos
+# como ContaCorrente e etc. Ele devolveria uma copia da referência do objeto e não uma copia do proprio objeto.
+assistiram = usuarios_data_science.copy()
+assistiram.extend(usuarios_machine_learning)
+
+print(assistiram)
+print(len(assistiram))
+
+
+print("\n=======================|| Sobre o tipo conjunto ou set() ||=======================\n")
+
+# O conjunto ou set(), tem como caracteristica nao aceitar a repeticao de valores.
+# Ele também não possui um indice e não é ordenado
+assistiram_set = set(assistiram)
+print(assistiram_set)
+
+
+print("\n=======================|| Passando uma lista ou iteravel no construtor de set() ||=======================\n")
+
+set_com_lista = set([1, 2, 3, 1])
+print(set_com_lista)
+
+numeros = [15, 23, 45, 56, 89, 47, 23, 45] # Criando um lista simples de inteiros
+numeros_ordenados = list(enumerate(numeros)) # Criando uma lista de tuplas onde a chave é o indice
+print(numeros_ordenados)
+
+conjunto_ordenado = set(numeros_ordenados) # Criando um set com uma lista de tuplas
+print(conjunto_ordenado)
+
+# Note que a lista de numeros repete o valor 23 no indice 1 e 6, porém quando geramos um set com a lista de numeros,
+# mesmo assim ele criou duas tuplas com os valores: (1, 23) e (6, 23).
+# O set() so aceitou a repetira por que a chave da tupla (key) é diferente entre as duas.
+# Caso a inteção fosse mudar esse comportamentos, seria necessario criar uma classe que herda de set()
+# e sobrescrever o metodo de comparacao, atribuindo a nossa logica
+
+
+print("\n=======================|| Instanciando um set() direto com os valores ||=======================\n")
+
+set_sem_lista = {1, 2, 3, 1}
+print(set_sem_lista)
+
+print(type({1, 2}))
+
+
+print("\n=======================|| Retorna error caso tente acessar um indice de um set()||=======================\n")
+
+# O conjunto nao tem indice, nao sendo possivel pegar um elemento pelo indice
+conjunto = {4, 89, 16, 29, 0}
+try:
+    print(conjunto[1])
+except TypeError as te:
+    print(f'Error: {te}')
+
+
+print("\n=======================|| Iterando sobre um set() ||=======================\n")
+
+usuarios = {15, 23, 45, 56, 89, 47, 23, 45}
+print(usuarios)
+for usuario in usuarios:
+    print(usuario)
+
+
+print("\n=======================|| Unindo dois conjuntos ||=======================\n")
+
+
+usuarios_data_science = {15, 23, 43, 56}
+usuarios_machine_learning = {13, 23, 56, 42}
+
+# O set(0 não possui um metodo built-in como o extend(0 da lista,
+# porem podemos utilizar o operador de ou (|) para atribuir duas listas há um set()
+usuarios_nos_dois_cursos = set(usuarios_data_science | usuarios_machine_learning)
+print(usuarios_nos_dois_cursos)
