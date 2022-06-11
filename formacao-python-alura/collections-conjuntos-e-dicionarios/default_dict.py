0cba9083e607d01461e97c55692bd97b419ee7fb
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-06-11 15:12:49 -0300

    sobre o Counter()

--- formacao-python-alura/collections-conjuntos-e-dicionarios/default_dict.py
+++ formacao-python-alura/collections-conjuntos-e-dicionarios/default_dict.py
@@ -15,7 +15,6 @@ for palavra in meu_texto.split():
 
 print(aparicoes)
 
-
 print("\n=========|| Contando o numero de repeticoes de cada palavra em um texto usando o defaultdict()||=========\n")
 
 meu_texto = 'Bem vindo meu nome é Renato eu gosto muito de nomes e tenho o meu cachorro e gosto muito de cachorro'
@@ -41,3 +40,52 @@ print(f'Contem a palavra \'Guilherme\'?: {dicionario["Guilherme"]}')
 dicionario["Guilherme"] = 15
 
 print(f'Contem a palavra \'Guilherme\'?: {dicionario["Guilherme"]}')
+
+print("\n=========|| Melhorando a implementacao acima ||=========\n")
+
+meu_texto = 'Bem vindo meu nome é Renato eu gosto muito de nomes e tenho o meu cachorro e gosto muito de cachorro'
+meu_texto = meu_texto.lower()
+
+dicionario = defaultdict(int)
+
+for palavra in meu_texto.split():
+    dicionario[palavra] += 1
+
+print(dicionario)
+
+print("\n=========|| Usando o Counter() ||=========\n")
+
+from collections import Counter
+
+meu_texto = 'Bem vindo meu nome é Renato eu gosto muito de nomes e tenho o meu cachorro e gosto muito de cachorro'
+meu_texto = meu_texto.lower()
+
+dicionario = Counter()
+for palavra in meu_texto.split():
+    dicionario[palavra] += 1
+
+print(dicionario)
+
+print("\n=========|| Usando o Counter() recebendo um iteravel||=========\n")
+
+meu_texto = 'Bem vindo meu nome é Renato eu gosto muito de nomes e tenho o meu cachorro e gosto muito de cachorro'
+meu_texto = meu_texto.lower()
+
+# Note que podemos passar uma lista no construtor e a resposta sera o counter dos elementos da lista
+dicionario = Counter(meu_texto.split())
+print(dicionario)
+
+print("\n=========|| Exemplo de uso  ||=========\n")
+
+
+# Note que nessa abordagem, se ele nao encontra o elemento no dicionario ele já chama a classe
+# e o seu construtor e instancia um objeto no dicionario
+class Conta:
+    def __init__(self):
+        print("Criando uma conta")
+
+
+contas = defaultdict(Conta)
+contas[15]
+contas[17]
+print(contas)
