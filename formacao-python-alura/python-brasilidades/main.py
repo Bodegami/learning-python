731fea94376968be397a63ccf87384e2f8555c91
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-06-12 20:14:17 -0300

    validando CNPJ

--- formacao-python-alura/python-brasilidades/main.py
+++ formacao-python-alura/python-brasilidades/main.py
@@ -1,14 +1,19 @@
-from Cpf import Cpf
-from validate_docbr import CPF
+from cpf_cnpj import CpfCnpj
+from validate_docbr import CPF, CNPJ
 
-
-cpf = Cpf("11111111112")
-print(cpf)
-
-
-
+print("=================|| Construcao da classe Cpf ||=====================")
+# cpf = Cpf("11111111112")
+# print(cpf)
 
 # cpf = "15616987913"
 # objeto_cpf = Cpf(cpf)
 #
 # print(objeto_cpf)
+
+print("=================|| Construcao da classe Cnpj ||=====================")
+
+exemplo_cnpj = "35379838000112"
+# cnpj = CNPJ()
+# print(cnpj.validate(exemplo_cnpj))
+tipo_documento = "cnpj"
+documento = CpfCnpj(exemplo_cnpj, tipo_documento)
