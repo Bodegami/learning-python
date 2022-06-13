6f9c801ca4e886ab17faa5a1c34e1b4bda086690
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-06-12 22:38:21 -0300

    refatorando o codigo

--- formacao-python-alura/python-brasilidades/main.py
+++ formacao-python-alura/python-brasilidades/main.py
@@ -1,4 +1,4 @@
-from cpf_cnpj import CpfCnpj
+from documento import Documento, DocCnpj, DocCpf
 from validate_docbr import CPF, CNPJ
 
 print("=================|| Construcao da classe Cpf ||=====================")
@@ -12,14 +12,25 @@ print("=================|| Construcao da classe Cpf ||=====================")
 
 print("=================|| Construcao da classe Cnpj ||=====================")
 
-exemplo_cnpj = "35379838000112"
+# exemplo_cnpj = "35379838000112"
+# exemplo_cpf = "32007832062"
+# # cnpj = CNPJ()
+# # print(cnpj.validate(exemplo_cnpj))
+# tipo_documento = "cnpj"
+# documento = CpfCnpj(exemplo_cnpj, tipo_documento)
+# print(documento)
+#
+# tipo_documento = "cpf"
+# documento = CpfCnpj(exemplo_cpf,tipo_documento)
+# print(documento)
+
+
+print("=================|| Refatorando a classe ||=====================")
+
 exemplo_cpf = "32007832062"
-# cnpj = CNPJ()
-# print(cnpj.validate(exemplo_cnpj))
-tipo_documento = "cnpj"
-documento = CpfCnpj(exemplo_cnpj, tipo_documento)
+documento = Documento.cria_documento(exemplo_cpf)
 print(documento)
 
-tipo_documento = "cpf"
-documento = CpfCnpj(exemplo_cpf,tipo_documento)
+exemplo_cnpj = "35379838000112"
+documento = Documento.cria_documento(exemplo_cnpj)
 print(documento)
