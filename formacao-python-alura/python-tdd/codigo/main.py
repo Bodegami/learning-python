d7e0e78487a2d8e0529a9703cd18e11d198791e4
Renato <rena.f.oliveira@gmail.com>
2022-09-30 22:04:41 -0300

    primeiro teste

--- formacao-python-alura/python-tdd/codigo/main.py
+++ formacao-python-alura/python-tdd/codigo/main.py
@@ -1,5 +1,12 @@
 from codigo.bytebank import Funcionario
 
-lucas = Funcionario('Lucas Carvalho', '13/03/2000', 1000)
+#lucas = Funcionario('Lucas Carvalho', '13/03/2000', 1000)
 
-print(lucas.idade())
+#print(lucas.idade())
+
+
+def teste_idade():
+    functionario_teste = Funcionario('Teste', '13/03/2000', 1111)
+    print(f'Teste = {functionario_teste.idade()}')
+
+teste_idade()
