4506a41e8ad3c70b6981ca342d4d5b0c100b1277
Renato <rena.f.oliveira@gmail.com>
2023-06-17 21:34:08 -0300

    comparando variaveis

--- revisao-python/jogos/adivinhacao.py
+++ revisao-python/jogos/adivinhacao.py
@@ -1,1 +1,20 @@
+print("*********************************")
 print("Bem vindo no jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+#O input alem de imprimir a frase, também recebe a entrada
+#O porem ele sempre recebe a entrada como String
+chute_str = input("Digite o seu numero: ")
+
+print("Você digitou ", chute_str)
+
+chute = int(chute_str)
+
+if(numero_secreto == chute):
+    print("Você acertou!")
+else:
+    print("Você errou!")
+
+print("Fim do jogo!")
