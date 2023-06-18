daaa8b6b90482ddb727173d69481fdaa492f63b2
Renato <rena.f.oliveira@gmail.com>
2023-06-18 16:23:51 -0300

    O laço com while

--- revisao-python/jogos/adivinhacao.py
+++ revisao-python/jogos/adivinhacao.py
@@ -3,25 +3,30 @@ print("Bem vindo no jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-#O input alem de imprimir a frase, também recebe a entrada
-#O porem ele sempre recebe a entrada como String
-chute_str = input("Digite o seu numero: ")
+while (rodada <= total_de_tentativas):
+    print(f'Tentativa {rodada} de {total_de_tentativas}')
 
-print("Você digitou ", chute_str)
+    #O input alem de imprimir a frase, também recebe a entrada sempre como String
+    chute_str = input("Digite o seu numero: ")
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
 
-chute = int(chute_str)
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+    if(acertou):
+        print("Você acertou!")
+        break
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
 
-if(acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior do que o número secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor do que o número secreto.")
+    rodada += 1
 
 print("Fim do jogo!")
