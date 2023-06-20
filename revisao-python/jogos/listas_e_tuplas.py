fe95c5f210975192c89e86cb8230e986de354e0d
Renato <rena.f.oliveira@gmail.com>
2023-06-19 22:22:14 -0300

    Listas e tuplas juntas

--- revisao-python/jogos/listas_e_tuplas.py
+++ revisao-python/jogos/listas_e_tuplas.py
@@ -21,5 +21,24 @@ print("Formato: {} - Values: {}".format(type(dias), dias))
 dias = ("S", "T", "Q", "Q", "S", "S", "D")
 print("Formato: {} - Values: {}".format(type(dias), dias))
 
+p1 = (3, 5)
+p2 = (4, 6)
+p3 = (5, 7)
+line = [p1, p2, p3]
+print(line)
+
+p1 = ("Nico", 39)
+p2 = ("Flavio", 37)
+instrutores = [p1, p2]
+print(instrutores)
+print("Buscando a idade da primeira tupla dentro da lista: {}".format(instrutores[0][1]))
 
 
+palavras = []
+palavras.append("banana")
+palavras.append("abacaxi")
+nova = tuple(palavras)
+print(nova)
+
+outra = list(nova)
+print(outra)
