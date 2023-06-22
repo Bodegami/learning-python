0a06d140d8583a1601047d39ce1d3350359ded86
Renato <rena.f.oliveira@gmail.com>
2023-06-21 22:59:01 -0300

    Dados e comportamento

--- revisao-python/oo/main.py
+++ revisao-python/oo/main.py
@@ -1,9 +1,24 @@
 
-from teste import cria_conta
+from teste import cria_conta, deposita, saca, extrato
 
-conta = cria_conta(123, "Renato", 500.0, 1000.0)
+conta = cria_conta(123, "Renato", 55.0, 1000.0)
 print(conta)
-print(conta["numero"])
+print(conta["saldo"])
+
+deposita(conta, 15.0)
+extrato(conta)
+
+saca(conta, 20.0)
+extrato(conta)
+
+
+conta2 = cria_conta(321, 200.0)
+deposita(conta2, 200.0)
+extrato(conta2)
+
+
+conta3 = cria_conta(titular="Nico", limite=1000.0)
+deposita(conta3, 100.0)
 
 
 if __name__ == '__main__':
