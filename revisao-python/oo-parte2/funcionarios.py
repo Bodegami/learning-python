7e36551f0f4791700547382a2e600134598dec46
Renato <rena.f.oliveira@gmail.com>
2023-07-01 23:22:39 -0300

    Mixins && conclusao curso python-avancando-na-orientacao-a-objetos

--- revisao-python/oo-parte2/funcionarios.py
+++ revisao-python/oo-parte2/funcionarios.py
@@ -1,4 +1,7 @@
 class Funcionario:
+    def __init__(self, nome):
+        self.nome = nome
+
     def registra_horas(self, horas):
         print('Horas registradas...')
 
@@ -22,6 +25,12 @@ class Alura(Funcionario):
         print('Mostrando perguntas não respondidas do fórum')
 
 
+class Hipster:
+
+    def __str__(self):
+        return f'Hipster, {self.nome}'
+
+
 class Junior(Alura):
     pass
 
@@ -30,11 +39,15 @@ class Pleno(Alura, Caelum):
     pass
 
 
-jose = Junior()
+class Senior(Alura, Caelum, Hipster):
+    pass
+
+
+jose = Junior("jose")
 jose.busca_perguntas_sem_resposta()
 jose.mostrar_tarefas()
 
-luan = Pleno()
+luan = Pleno("luan")
 luan.busca_perguntas_sem_resposta()
 luan.busca_cursos_do_mes()
 
@@ -42,3 +55,10 @@ luan.mostrar_tarefas()
 
 # algoritmo MRO Method Resolution
 # pleno -> Alura -> Funcionario -> Caelum -> Funcionario
+
+mauro = Senior("mauro")
+print(mauro)
+
+# MIXIN
+# são classes pequenas que compartilham metodos auxiliares para outras classes
+# obs: a ideia que não tenha a necessidade de instanciar esse tipo de classe
