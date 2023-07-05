9b615b61d0b88a07685f01b92c37d00d542cb9ed
Renato <rena.f.oliveira@gmail.com>
2023-07-04 22:21:48 -0300

    Quantificadores e intervalos

--- revisao-python/extrator-url/extrator_cep.py
+++ revisao-python/extrator-url/extrator_cep.py
@@ -5,7 +5,7 @@ endereco = "Rua da Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ,
 
 # 5 digitos + hífen (opcional) + digitos
 
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}") # poderia ser "[0-9]{5}[-]?[0-9]{3}"
 busca = padrao.search(endereco) # Retorna um Match ou None caso não encontre
 
 if busca:
