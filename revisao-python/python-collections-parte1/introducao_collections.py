ca2e0b7ebff82a1892a10e15ac58821e8e11e259
Renato <rena.f.oliveira@gmail.com>
2023-07-04 23:48:30 -0300

    Introdução a coleções e lista

---
+++ revisao-python/python-collections-parte1/introducao_collections.py
@@ -0,0 +1,34 @@
+idade1 = 39
+idade2 = 30
+idade3 = 27
+idade4 = 18
+
+idades = [39, 30, 27, 18]
+
+print(type(idades))
+print(idades)
+
+print(len(idades))
+
+print(idades[2])
+
+idades.append(15)
+
+print(idades)
+
+print(idades[4])
+
+for idade in idades:
+    print(idade)
+
+idades.append(27) # Adiciona um elemento no final da lista
+
+print(idades)
+
+idades.remove(27) # remove o primeiro elemento que encontrar na lista
+
+print(idades)
+
+idades.clear() # Remove todos elementos da lista
+
+print(idades)
