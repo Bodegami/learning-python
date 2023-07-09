957c43adf2a3281dfb42224197f4600fed0e8aad
Bodegami <rena.f.oliveira@gmail.com>
2023-07-08 23:59:56 -0300

    Pacotes python

--- revisao-python/python-brasilidades/main.py
+++ revisao-python/python-brasilidades/main.py
@@ -1,8 +1,12 @@
 from Cpf import Cpf
+from validate_docbr import CPF
 
 cpf = str(15616957801)
 
-novo_cpf = Cpf(cpf)
-print(novo_cpf)
+# novo_cpf = Cpf(cpf)
+# print(novo_cpf)
+
+validador = CPF()
+print(validador.validate(cpf))
 
 
