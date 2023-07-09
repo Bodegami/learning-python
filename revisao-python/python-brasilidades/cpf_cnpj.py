189ac34257d7d1c1431e5192fc080b0c0fe27903
Bodegami <rena.f.oliveira@gmail.com>
2023-07-09 00:30:58 -0300

    Validando CNPJ

---
+++ revisao-python/python-brasilidades/cpf_cnpj.py
@@ -0,0 +1,44 @@
+from validate_docbr import CPF, CNPJ
+
+
+class CpfCnpj:
+
+    def __init__(self, documento, tipo_documento):
+        self.tipo_documento = tipo_documento
+        documento = str(documento)
+        if self.tipo_documento == "cpf":
+            if self.cpf_eh_valido(documento):
+                self.documento = documento
+            else:
+                raise ValueError("CPF inválido!")
+        elif self.tipo_documento == "cnpj":
+            if self.cnpj_eh_valido(documento):
+                self.documento = documento
+            else:
+                raise ValueError("CNPJ inválido!")
+        else:
+            raise ValueError("Documento inválido!")
+
+    def __str__(self):
+        return self.format_cpf()
+
+    def cpf_eh_valido(self, documento):
+        if len(documento) == 11:
+            validador = CPF()
+            return validador.validate(documento)
+        else:
+            raise ValueError("Quantidade de digitos inválida!!")
+
+    def cnpj_eh_valido(self, documento):
+        if len(documento) == 14:
+            validador = CNPJ()
+            return validador.validate(documento)
+        else:
+            raise ValueError("Quantidade de digitos inválida!!")
+
+    def format_cpf(self):
+        mascara = CPF()
+        return mascara.mask(self.documento)
+
+
+
--- revisao-python/python-brasilidades/main.py
+++ revisao-python/python-brasilidades/main.py
@@ -1,10 +1,12 @@
-from Cpf import Cpf
-from validate_docbr import CPF
+from cpf_cnpj import CpfCnpj
 
 cpf = str(83977647040)
-
-novo_cpf = Cpf(cpf)
+novo_cpf = CpfCnpj(cpf, "cpf")
 print(novo_cpf)
+
+cnpj = str(55874162000140)
+novo_cnpj = CpfCnpj(cnpj, "cnpj")
+print(novo_cnpj)
 
 
 
