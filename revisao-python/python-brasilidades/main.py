77b1d27ea0527c12f7fe22ed40a2a313f2d1d56c
Bodegami <rena.f.oliveira@gmail.com>
2023-07-09 21:13:58 -0300

    Refatorando o código

--- revisao-python/python-brasilidades/main.py
+++ revisao-python/python-brasilidades/main.py
@@ -1,12 +1,18 @@
-from cpf_cnpj import CpfCnpj
+from cpf_cnpj import CpfCnpj, Documento
 
 cpf = str(83977647040)
-novo_cpf = CpfCnpj(cpf, "cpf")
-print(novo_cpf)
+# novo_cpf = CpfCnpj(cpf, "cpf")
+# print(novo_cpf)
 
 cnpj = str(55874162000140)
-novo_cnpj = CpfCnpj(cnpj, "cnpj")
-print(novo_cnpj)
+# novo_cnpj = CpfCnpj(cnpj, "cnpj")
+# print(novo_cnpj)
+
+documento = Documento.cria_documento(cnpj)
+print(documento)
+
+documento = Documento.cria_documento(cpf)
+print(documento)
 
 
 
