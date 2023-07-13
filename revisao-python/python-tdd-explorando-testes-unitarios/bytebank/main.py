fdb1ecf814af7e33dba979ae5b49dd03100c22b1
Bodegami <rena.f.oliveira@gmail.com>
2023-07-12 21:48:02 -0300

    Criando o primeiro teste

--- revisao-python/python-tdd-explorando-testes-unitarios/bytebank/main.py
+++ revisao-python/python-tdd-explorando-testes-unitarios/bytebank/main.py
@@ -3,3 +3,10 @@ from codigo.bytebank import Funcionario
 lucas = Funcionario(nome='Lucas Carvalho', data_nascimento='13/03/2000', salario=1000)
 
 print(lucas.idade())
+
+def teste_idade():
+    funcionario_teste = Funcionario('Teste', '13/03/2000', 1111)
+
+    print(f'Teste = {funcionario_teste.idade()}')
+
+teste_idade()
