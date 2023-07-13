00c044cfcd90b557e8b3d3665de72bf82e782493
Bodegami <rena.f.oliveira@gmail.com>
2023-07-12 21:42:28 -0300

    Sobre o projeto bytebank

--- revisao-python/python-tdd-explorando-testes-unitarios/bytebank/main.py
+++ revisao-python/python-tdd-explorando-testes-unitarios/bytebank/main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+from codigo.bytebank import Funcionario
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+lucas = Funcionario(nome='Lucas Carvalho', data_nascimento='13/03/2000', salario=1000)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(lucas.idade())
