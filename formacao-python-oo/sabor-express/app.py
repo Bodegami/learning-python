e4fccbb85f80288c69f833beb90b92d8f328e805
Renato <rena.f.oliveira@gmail.com>
2024-02-01 11:39:30 -0300

    aprofundando em funcoes

--- formacao-python-oo/sabor-express/app.py
+++ formacao-python-oo/sabor-express/app.py
@@ -1,33 +1,46 @@
 import os
 
-print("""
+
+def exibir_nome_do_programa():
+    print("""
 
 █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
 ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
-        """)
+""")
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def exibir_opcoes():
+   print('1. Cadastrar restaurante')
+   print('2. Listar restaurante')
+   print('3. Ativar restaurante')
+   print('4. Sair\n')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-# O Retorno da funcao input() sempre é do tipo str
-#opcao_escolhida = int(opcao_escolhida)
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    # O Retorno da funcao input() sempre é do tipo str
+    #opcao_escolhida = int(opcao_escolhida)
 
-print(f'Você escolheu a opção: {opcao_escolhida}')
+    print(f'Você escolheu a opção: {opcao_escolhida}')
+
+
+    if opcao_escolhida == 1:
+       print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+       print('Listar restaurante')
+    elif opcao_escolhida == 3:
+       print('Ativar restaurante')
+    else:
+       finalizar_app() 
 
 def finalizar_app():
     os.system('cls')
-    print('Finalizando o app!\n')
+    print('Finalizando o app!\n')   
 
+    
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+        
 
-
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('Listar restaurante')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else:
-    finalizar_app()           
+if __name__ == '__main__':
+    main()
