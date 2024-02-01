ec3cf1fe7b5faad03dd66fd0ebc5f25efe40f1c6
Renato <rena.f.oliveira@gmail.com>
2024-02-01 20:38:49 -0300

    metodos especiais

--- formacao-python-oo/sabor-express-oo/modelos/restaurante.py
+++ formacao-python-oo/sabor-express-oo/modelos/restaurante.py
@@ -1,9 +1,14 @@
 
 class Restaurante:
+    # Self é a referencia da instancia atual que esta sendo invocada
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+
+    # metodo __str__ retorna a representação textual do objeto
+    def __str__(self):
+        return f'Nome: {self.nome} | Categoria: {self.categoria} | Ativo: {self.ativo}'    
 
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
@@ -12,5 +17,8 @@ restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 restaurantes = [restaurante_praca, restaurante_pizza]
 
 #print(dir(restaurantes))
-print(vars(restaurante_praca))
-print(vars(restaurante_pizza))
+#print(vars(restaurante_praca))
+#print(vars(restaurante_pizza))
+
+print(restaurante_praca)
+print(restaurante_pizza)
