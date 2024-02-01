7f42606b29d78955464b720215f275ba33780ad0
Renato <rena.f.oliveira@gmail.com>
2024-02-01 20:48:25 -0300

    criando meus metodos

--- formacao-python-oo/sabor-express-oo/modelos/restaurante.py
+++ formacao-python-oo/sabor-express-oo/modelos/restaurante.py
@@ -1,24 +1,27 @@
 
 class Restaurante:
+    restaurantes = []
+
     # Self é a referencia da instancia atual que esta sendo invocada
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
 
     # metodo __str__ retorna a representação textual do objeto
     def __str__(self):
-        return f'Nome: {self.nome} | Categoria: {self.categoria} | Ativo: {self.ativo}'    
+        return f'Nome: {self.nome} | Categoria: {self.categoria} | Ativo: {self.ativo}'  
+
+    def listar_restaurantes():
+        [print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}') for restaurante in Restaurante.restaurantes]
 
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+Restaurante.listar_restaurantes()
 
 #print(dir(restaurantes))
 #print(vars(restaurante_praca))
 #print(vars(restaurante_pizza))
-
-print(restaurante_praca)
-print(restaurante_pizza)
