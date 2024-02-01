dc3a161359d3aedc7fa84c7eefe27e1198f504ff
Renato <rena.f.oliveira@gmail.com>
2024-02-01 12:06:49 -0300

    try except

--- formacao-python-oo/sabor-express/app.py
+++ formacao-python-oo/sabor-express/app.py
@@ -14,33 +14,42 @@ def exibir_opcoes():
    print('3. Ativar restaurante')
    print('4. Sair\n')
 
+def opcao_invalida():
+    print('Opção Inválida!')
+    input('Digite um tecla para voltar ao menu principal')
+    main()
+
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    # O Retorno da funcao input() sempre é do tipo str
-    #opcao_escolhida = int(opcao_escolhida)
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # O Retorno da funcao input() sempre é do tipo str
+        #opcao_escolhida = int(opcao_escolhida)
 
-    print(f'Você escolheu a opção: {opcao_escolhida}')
+        print(f'Você escolheu a opção: {opcao_escolhida}')
+        #    print(1 < opcao_escolhida <= 3) -> opcao escolhida 2
 
-#    if opcao_escolhida == 1:
-#       print('Cadastrar restaurante')
-#    elif opcao_escolhida == 2:
-#       print('Listar restaurante')
-#    elif opcao_escolhida == 3:
-#       print('Ativar restaurante')
-#    else:
-#       finalizar_app() 
+        #    if opcao_escolhida == 1:
+        #       print('Cadastrar restaurante')
+        #    elif opcao_escolhida == 2:
+        #       print('Listar restaurante')
+        #    elif opcao_escolhida == 3:
+        #       print('Ativar restaurante')
+        #    else:
+        #       finalizar_app() 
 
-    match opcao_escolhida:
-        case 1:
-             print('Cadastrar restaurante')
-        case 2:
-             print('Listar restaurante')
-        case 3:
-             print('Ativar restaurante')                          
-        case 4:
-             finalizar_app()
-        case _:
-             print('Opção inválida!')             
+        match opcao_escolhida:
+            case 1:
+                print('Cadastrar restaurante')
+            case 2:
+                print('Listar restaurante')
+            case 3:
+                print('Ativar restaurante')                          
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()                 
 
 
 def finalizar_app():
@@ -49,6 +58,7 @@ def finalizar_app():
 
     
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
