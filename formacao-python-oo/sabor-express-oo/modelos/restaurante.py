30532d603062d9dfd4a59f6bee1f188ac450971a
Renato <rena.f.oliveira@gmail.com>
2024-02-01 22:52:10 -0300

    Property

--- formacao-python-oo/sabor-express-oo/modelos/restaurante.py
+++ formacao-python-oo/sabor-express-oo/modelos/restaurante.py
@@ -6,7 +6,7 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     # metodo __str__ retorna a representação textual do objeto
@@ -14,7 +14,12 @@ class Restaurante:
         return f'Nome: {self.nome} | Categoria: {self.categoria} | Ativo: {self.ativo}'  
 
     def listar_restaurantes():
-        [print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}') for restaurante in Restaurante.restaurantes]
+        print(f'{"Nome do restaurante".ljust(24)} | {"Categoria".ljust(25)} | {"Status"}')
+        [print(f'{restaurante.nome.ljust(25)}| {restaurante.categoria.ljust(25)} | {restaurante.ativo}') for restaurante in Restaurante.restaurantes]
+
+    @property
+    def ativo(self):
+           return '☒' if self._ativo else '☐'
 
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
