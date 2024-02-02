4d983f7f581564770deea87e83c1da6be79e0df3
Renato <rena.f.oliveira@gmail.com>
2024-02-01 22:58:01 -0300

    aprofundando em propriedades

--- formacao-python-oo/sabor-express-oo/modelos/restaurante.py
+++ formacao-python-oo/sabor-express-oo/modelos/restaurante.py
@@ -4,26 +4,28 @@ class Restaurante:
 
     # Self é a referencia da instancia atual que esta sendo invocada
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
 
     # metodo __str__ retorna a representação textual do objeto
     def __str__(self):
-        return f'Nome: {self.nome} | Categoria: {self.categoria} | Ativo: {self.ativo}'  
+        return f'Nome: {self._nome} | Categoria: {self._categoria} | Ativo: {self._ativo}'  
 
     def listar_restaurantes():
         print(f'{"Nome do restaurante".ljust(24)} | {"Categoria".ljust(25)} | {"Status"}')
-        [print(f'{restaurante.nome.ljust(25)}| {restaurante.categoria.ljust(25)} | {restaurante.ativo}') for restaurante in Restaurante.restaurantes]
+        [print(f'{restaurante._nome.ljust(25)}| {restaurante._categoria.ljust(25)} | {restaurante._ativo}') for restaurante in Restaurante.restaurantes]
 
     @property
     def ativo(self):
            return '☒' if self._ativo else '☐'
 
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_pizza = Restaurante('pizza Express', 'Italiana')
+
+restaurante_pizza.nome = 'Pizza 21'
 
 Restaurante.listar_restaurantes()
 
