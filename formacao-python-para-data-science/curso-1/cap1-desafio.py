bccfccd3f4fb20f29003e9a212d0787ee927ce09
Renato <rena.f.oliveira@gmail.com>
2024-02-04 23:57:29 -0300

    desafio capitulo 1

---
+++ formacao-python-para-data-science/curso-1/cap1-desafio.py
@@ -0,0 +1,26 @@
+#1
+print('Escola de dados Alura!')
+print()
+
+nome = 'Renato'
+sobrenome = 'Ferreira'
+
+#2
+print(f'Nome: {nome}')
+print(f'Sonrenome: {sobrenome}')
+print()
+
+#3
+[print(caracter) for caracter in nome]
+print()
+
+#4
+print(4, 'novembro', 1986)
+print()
+
+#5
+ano = 2024
+print(f'Ano atual: {ano}')
+
+
+
