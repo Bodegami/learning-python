a02c642a5e967e22b73667cf2827e150f4d722f2
Renato <rena.f.oliveira@gmail.com>
2024-03-24 23:17:42 -0300

    lista de tuplas

---
+++ formacao-python-para-data-science/curso-2/lista_de_tuplas.py
@@ -0,0 +1,15 @@
+from random import randint
+
+def gera_codigo():
+  return str(randint(0,999))
+
+
+estudantes = ["João", "Maria", "José", "Cláudia", "Ana"]
+print(estudantes)
+
+codigo_estudantes = []
+
+for i in range(len(estudantes)):
+  codigo_estudantes.append((estudantes[i], estudantes[i][0] + gera_codigo()))
+
+print(codigo_estudantes)
