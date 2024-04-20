e97466929275cf1b5ab9a21bfe0109a276619626
Renato <rena.f.oliveira@gmail.com>
2024-04-20 11:38:05 -0300

    desafio dict comprehension com if else

--- formacao-python-para-data-science/curso-2/dict_comprehension.py
+++ formacao-python-para-data-science/curso-2/dict_comprehension.py
@@ -15,3 +15,32 @@ resultado = [f'Estudante: {cadastro["Estudante"][i]} | Média Final: {cadastro["
 
 [print(item + "\n") for item in resultado]
 
+"""
+    Desafio:
+
+    Recebemos uma demanda da instituição de ensino do nosso projeto que nos repassou uma lista de 20 estudantes e 
+    suas respectivas médias finais. Aqui, nós precisamos selecionar estudantes que tenham média final maior ou igual a 9.0. 
+    Esses(as) estudantes serão premiados(as) com uma bolsa de estudos para o próximo ano letivo.
+
+    Para filtrar os dados, temos que gerar um dicionário cujas chaves são os nomes e os valores são as médias dos(as) estudantes selecionados(as). 
+    Estes são os dados recebidos:
+
+"""
+
+nomes_estudantes = [ "Enrico Monteiro", "Luna Pereira", "Anthony Silveira", "Letícia Fernandes", 
+                    "João Vitor Nascimento", "Maysa Caldeira", "Diana Carvalho", "Mariane da Rosa",
+                    "Camila Fernandes", "Levi Alves", "Nicolas da Rocha", "Amanda Novaes", 
+                    "Laís Moraes", "Letícia Oliveira", "Lucca Novaes", "Lara Cunha", 
+                    "Beatriz Martins", "João Vitor Azevedo", "Stephany Rosa", "Gustavo Henrique Lima" ]
+
+medias_estudantes = [5.4, 4.1, 9.1, 5.3, 6.9, 3.1, 10.0, 5.0, 8.2, 5.5,
+                    8.1, 7.4, 5.0, 3.7, 8.1, 6.2, 6.1, 5.6, 6.7, 8.2]
+
+resultado_de_todos_alunos = {nomes_estudantes[i]: medias_estudantes[i] for i in range(len(nomes_estudantes))}
+print(resultado_de_todos_alunos)
+
+resultado_dos_aprovados= {nomes_estudantes[i]: medias_estudantes[i] for i in range(len(nomes_estudantes)) if medias_estudantes[i] >= 9.0}
+print(f"Aprovados para bolsa de estudos: {resultado_dos_aprovados}")
+
+bolsistas = {nome: media for nome, media in zip(nomes_estudantes, medias_estudantes) if media >= 9.0}
+print(bolsistas)
