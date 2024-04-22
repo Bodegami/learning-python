157cc68d0a7f7b8211ec5e4ca8a54cb2c259b7cf
Renato <rena.f.oliveira@gmail.com>
2024-04-21 22:33:27 -0300

    dimensoes dos arrays

--- formacao-python-para-data-science/curso-3/carregando_dados.py
+++ formacao-python-para-data-science/curso-3/carregando_dados.py
@@ -4,12 +4,27 @@ import numpy as np
 colunas = np.arange(1,88,1)
 
 # carrega arquivo csv, com delimitador e definicao de colunas 
-tabela_arquivo = np.loadtxt('./numpy-dados/apples_ts.csv', delimiter=',', usecols=colunas)
-print(tabela_arquivo[0])
+dado_arquivo = np.loadtxt('./numpy-dados/apples_ts.csv', delimiter=',', usecols=colunas)
+print(dado_arquivo[0])
 
 url = 'https://raw.githubusercontent.com/alura-cursos/numpy/dados/apples_ts.csv'
 
 # carrega atabela de uma url, com delimitador e definicao de colunas 
-tabela_url = np.loadtxt(url, delimiter=',', usecols=colunas)
-print(tabela_url[0] == tabela_arquivo[0])
+dado_url = np.loadtxt(url, delimiter=',', usecols=colunas)
+print(dado_url[0] == dado_arquivo[0])
 
+dado = dado_url
+
+# ndim retorna o numero de dimensoes, aqui no caso 2 (linhas e colunas)
+print(dado.ndim)
+
+# size retorna o numero de elementos do dado
+print(dado.size)
+
+# shape retorna o numero de elementos por dimensoes, aqui no caso 2 (linhas e colunas)
+print(dado.shape)
+
+# T (transposicao da matematica) faz a conversão de linha para colunas e vice-versa
+print(dado.T)
+
+dado_transposto = dado.T
